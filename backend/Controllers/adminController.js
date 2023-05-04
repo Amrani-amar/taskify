789a26eb41c9e3ff2edfbe989e768cddaf871986
Amrani-amar <119869638+Amrani-amar@users.noreply.github.com>
2023-05-04 23:02:59 +0100

    Update adminController.js

--- backend/Controllers/adminController.js
+++ backend/Controllers/adminController.js
@@ -1,54 +1,6 @@
 import Taches from '../Models/Taches.js';
 
-// Fonctions pour les opérations CRUD sur le modèle User01
 
-// export const createUser01 = async (req, res) => {
-//   try {
-//     const user01 = new User01(req.body);
-//     await user01.save();
-//     res.status(201).json(user01);
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// };
-
-// export const getUser01 = async (req, res) => {
-//   try {
-//     const users = await User01.find({});
-//     res.status(200).json(users);
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// };
-
-// export const updateUser01 = async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const user01 = await User01.findByIdAndUpdate(id, req.body, {
-//       new: true,
-//       runValidators: true,
-//     });
-//     if (!user01) {
-//       return res.status(404).json({ error: 'User01 not found' });
-//     }
-//     res.status(200).json(user01);
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// };
-
-// export const deleteUser01 = async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const user01 = await User01.findByIdAndDelete(id);
-//     if (!user01) {
-//       return res.status(404).json({ error: 'User01 not found' });
-//     }
-//     res.status(200).json({ message: 'User01 deleted successfully' });
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// };
 
 // Fonctions pour les opérations CRUD sur le modèle Taches
 
